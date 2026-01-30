43998108b3e82231e2039cfc0ecf38023e755de7
Katal1na <katia.yakushova@gmail.com>
2026-01-30 15:06:56 +0200

    Add conversion function: kilometers to miles

--- src/app/Main.java
+++ src/app/Main.java
@@ -2,6 +2,7 @@ package app;
 
 public class Main {
     private static final double MILES_TO_KM = 1.60934;
+    private static final double KM_TO_MILES = 0.621371;
 
     public static void main(String[] args) {
         System.out.println("Converter App.");
@@ -10,9 +11,18 @@ public class Main {
         double miles = 5;
         double kilometers = convMilesToKilometers(miles);
         System.out.println(miles + " miles = " + kilometers + " kilometers");
+
+        double km = 8;
+        double milesResult = convKilometersToMiles(km);
+        System.out.println(km + " kilometers = " + milesResult + " miles");
     }
 
     private static double convMilesToKilometers(double miles) {
         return miles * MILES_TO_KM;
     }
+
+    private static double convKilometersToMiles(double kilometers) {
+        return kilometers * KM_TO_MILES;
+    }
 }
+
